78197e7b3faab4ca33651c0e3ac12c0bd00125a7
Mansi Kale <mansipk2007@gmail.com>
2026-06-13 22:17:34 +0530

    another file added

--- day-01/exercises.py
+++ day-01/exercises.py
@@ -2,4 +2,4 @@ import numpy as np
 arr=np.array([30,31,29,33,32,31,30])
 print("Max",arr.max())
 print("Min",arr.min())
-print(f"Mean":{arr.mean():.2f})
+print(f"Mean:{arr.mean():.2f}")
---
+++ day-01/outlier_demo.py
@@ -0,0 +1,6 @@
+import numpy as np
+normal=np.array([30,30,30,30,30])
+
+outlier=np.array([30,30,30,30,100])
+print("normal mean",normal.mean())
+print("outlier mean",outlier.mean())
