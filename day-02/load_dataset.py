d69dba28d2a19323ca51fcfbe980ba393adab241
Mansi Kale <mansipk2007@gmail.com>
2026-06-14 14:50:48 +0530

    day-2

---
+++ day-02/load_dataset.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df=pd.read_csv("day-02/sensor_dataset.csv")
+print(df)
---
+++ day-02/pandas_basics.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+data={
+    "temperature":[30,35,40],
+    "voltage":[5.0,4.8,4.9]
+    }
+
+df=pd.DataFrame(data)
+
+print(df)
